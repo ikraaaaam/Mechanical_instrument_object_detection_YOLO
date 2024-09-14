fe2a59aa7190e1828dfc4d8755e0865368a1555f
ikraaaaam <60174254+ikraaaaam@users.noreply.github.com>
2024-09-14 23:04:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,13 @@ st.sidebar.header("Upload an Image")
 uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Read and resize the image
+    # Read and display the image
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image (Original Size)', use_column_width=True)
     
     # Resize the image to a standard size (e.g., 640x640 pixels)
     standard_size = (640, 640)
-    resized_image = image.resize(standard_size, Image.ANTIALIAS)
+    resized_image = image.resize(standard_size, Image.Resampling.LANCZOS)
     st.image(resized_image, caption='Resized Image', use_column_width=True)
     
     # Convert resized image to numpy array for model input
@@ -31,5 +31,3 @@ if uploaded_file is not None:
     # Draw results (the ultralytics library handles plotting)
     annotated_img = results[0].plot()  # Results are annotated by YOLOv8
     st.image(annotated_img, caption='Detected Objects', use_column_width=True)
-
-
